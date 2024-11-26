3a0e537f8ffff9d3c1e3670f48c70317b27ae19b
AHpxMac <ahpx@yandex.com>
2024-11-26 17:39:44 +0800

    clean: clean up test codes

--- praise_go/main.py
+++ praise_go/main.py
@@ -2,17 +2,15 @@ import datetime
 from fastapi import FastAPI
 import platform
 
-from src.utils.image_manipulation import compressImgToBase64
+from praise_go.utils.image_manipulation import compressImgToBase64
 
-# api = FastAPI()
+api = FastAPI()
 
-# @api.get('/')
-# def root():
-#     return {
-#         "arch": platform.machine(),
-#         "python": platform.python_version(),
-#         "os": platform.platform(),
-#         "time": datetime.datetime.now()
-#     }
-    
-compressImgToBase64("/Users/a1/Misc/Snipshots/Snipaste_2024-11-20_23-03-50.png")
+@api.get('/')
+def root():
+    return {
+        "arch": platform.machine(),
+        "python": platform.python_version(),
+        "os": platform.platform(),
+        "time": datetime.datetime.now()
+    }
