9f3a32fa71f9497428637418fad7ed63c819d85c
AHpxMac <ahpx@yandex.com>
2024-11-26 10:58:00 +0800

    feat: add root api for checking info

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import datetime
+from fastapi import FastAPI
+import platform 
+
+api = FastAPI()
+
+@api.get('/')
+def root():
+    return {
+        "arch": platform.machine(),
+        "python": platform.python_version(),
+        "os": platform.platform(),
+        "time": datetime.datetime.now()
+    }
